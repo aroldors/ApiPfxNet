21d9901a2147f02b7285bf644daed84206b4ad2c
Aroldo Silva <aroldomxm@gmail.com>
2024-10-15 16:25:59 -0300

    Customização do arquivo Program para execução da aplicação

--- DecriptPfx/Program.cs
+++ DecriptPfx/Program.cs
@@ -7,14 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+/*
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
+builder.WebHost.UseUrls($"http://*:{port}");
+*/
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
